0fd2b292ac8439ada23b43aeaf879a7558f186cc
maybites <registered@maybites.ch>
2026-08-08 15:00:27 +0200

    update to new decorator footprint

--- barn/haybale-visiongraph/haybale_visiongraph/__init__.py
+++ barn/haybale-visiongraph/haybale_visiongraph/__init__.py
@@ -27,7 +27,7 @@ from haywire.ui.widget.registry import WidgetRegistry
     author_url="https://author_url",
     dependencies=['haybale_core'],
     tags=['vision', 'camera', 'video', 'opencv'],
-    needs_refresh=True,
+    on_reload="restart",
     file_watcher=False,
 )
 class Library(BaseLibrary):
